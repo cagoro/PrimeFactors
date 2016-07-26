26c25e42b46c2e470814bf54939794e63c2cdfa4
Carlos Gonzalez Rodriguez <cgonzalezrodriguez@vistaprint.com>
2016-07-26 10:37:35 +0200

    TDD session with Alex Borisov (Kata completed)

---
+++ PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Primes
+{
+    public class PrimeFactors
+    {
+        public static List<int> Generate(int number)
+        {
+            var factors = new List<int>();
+            int divisor = 2;
+            while (number != 1)
+            {
+                while (number % divisor == 0)
+                {
+                    factors.Add(divisor);
+                    number = number / divisor;
+                }
+                divisor++;
+            }
+            return factors;
+        }
+    }
+}
---
+++ PrimeFactorsUnitTests/PrimeFactorsShould.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Primes;
+
+namespace PrimeFactorsUnitTests
+{
+    [TestFixture]
+    public class PrimeFactorsShould
+    {
+        private static Object[] CreateExpectedPrimeFactorsFor(int number, params int[] factors)
+        {
+            return new object[] {number, factors.ToList()};
+        }
+
+        private static readonly object[] FACTORS =
+        {
+            CreateExpectedPrimeFactorsFor(2, 2),
+            CreateExpectedPrimeFactorsFor(3, 3),
+            CreateExpectedPrimeFactorsFor(4, 2, 2),
+            CreateExpectedPrimeFactorsFor(6, 2, 3),
+            CreateExpectedPrimeFactorsFor(8, 2, 2, 2),
+            CreateExpectedPrimeFactorsFor(1296, 2, 2, 2, 2, 3, 3, 3, 3),
+            CreateExpectedPrimeFactorsFor(30, 2, 3, 5),
+            CreateExpectedPrimeFactorsFor(53130, 2, 3, 5, 7, 11, 23)
+        };
+
+        [Test, TestCaseSource("FACTORS")]
+        public void return_the_prime_factors_of_the_passed_number(int number, List<int> expectedFactors)
+        {
+            List<int> factors = PrimeFactors.Generate(number);
+            Assert.AreEqual(factors, expectedFactors);
+        }        
+    }
+}
